b9fbcbea717ed390c98d5bb88e427eda0923c226
Charles <babouwitonze921@gmail.com>
2021-07-14 13:56:05 +0200

    initial push

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
+export default function UUID_G(){
+  return(
+      <>
+      <title> Simple UUID_Generator</title>
+      <body>
+          
+          
+          <h1 class="title">Welcome to UUID_Generator</h1>
+      
+      
+      
+      
+      </body>
+      </>
+    )
+}
+      
